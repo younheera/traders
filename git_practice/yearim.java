46424ac5e245fbdea6a8fb21ae0f817b3d57016c
yearim jeong <r1730@naver.com>
2023-09-20 10:00:06 +0900

    feat. Add modify date

--- git_practice/yearim.java
+++ git_practice/yearim.java
@@ -1,5 +1,10 @@
 /**
  * @author jeongyearim
+ * @create date 2023-09-19 17:22:45
+ * @modify date 2023-09-19 17:22:45
+ */
+/**
+ * @author jeongyearim
  * @create date 2023-09-19 17:16:39
  * @modify date 2023-09-19 17:16:39
  */
