a7f6135be9cd62459bbd96d15d1fa05803571725
wheesunglee <wlee9486@naver.com>
2023-09-25 13:00:24 +0900

    refractor. Product 오류처리 및 반환타입 수정

---
+++ src/main/java/com/newus/traders/exception/CustomException.java
@@ -0,0 +1,18 @@
+/**
+ * @author wheesunglee
+ * @create date 2023-09-23 18:37:09
+ * @modify date 2023-09-23 18:37:09
+ */
+
+package com.newus.traders.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    
+    // 에러 상태와 메세지를 모두 담고 있는 ErrorCode 클래스를 담음
+    ErrorCode errorCode;
+}
--- src/main/java/com/newus/traders/product/repository/ProductRepository.java
+++ src/main/java/com/newus/traders/product/repository/ProductRepository.java
@@ -3,9 +3,12 @@
  * @create date 2023-09-19 08:20:08
  * @modify date 2023-09-19 08:20:08
  */
-package com.newus.traders.product;
+package com.newus.traders.product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.newus.traders.product.entity.Product;
+
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
