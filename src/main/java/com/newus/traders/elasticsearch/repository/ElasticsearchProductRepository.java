daf8e003fed35a74824e577340496a3258b9edbb
wheesunglee <wlee9486@naver.com>
2023-10-11 15:00:44 +0900

    feat. search using elastic search

---
+++ src/main/java/com/newus/traders/elasticsearch/repository/ElasticsearchProductRepository.java
@@ -0,0 +1,20 @@
+/**
+ * @author wheesunglee
+ * @create date 2023-09-27 11:08:20
+ * @modify date 2023-09-27 11:08:20
+ */
+
+package com.newus.traders.elasticsearch.repository;
+
+import com.newus.traders.elasticsearch.document.ProductDocument;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface ElasticsearchProductRepository extends ElasticsearchRepository<ProductDocument, String> {
+    @Query("{\"match\": {\"name.nori\": \"?0\"}}")
+    List<ProductDocument> findByName(String keyword);
+
+    ;
+}
