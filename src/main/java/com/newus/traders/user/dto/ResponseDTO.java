e17779abb187d349e3663b8536c44d18473d6de2
younheera <132442096+younheera@users.noreply.github.com>
2023-10-14 19:28:24 +0900

    Revert "feat. JWT토큰 로그인 및 회원가입 구현 방법변경"
    This reverts commit bd279f52acc319c86d95ad32cabbffff1d63bdd2.

--- src/main/java/com/newus/traders/user/dto/ResponseDTO.java
+++ src/main/java/com/newus/traders/user/dto/ResponseDTO.java
@@ -1,18 +1,18 @@
 package com.newus.traders.user.dto;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserDTO {
-	private String token;
-	private String email;
-	private String username;
-	private String password;
-	private String id;
+@Data
+public class ResponseDTO<T> {
+	private String error;
+	private List<T> data;
 }
+
+
--- src/main/java/com/newus/traders/user/repository/UserRepository.java
+++ src/main/java/com/newus/traders/user/repository/UserRepository.java
@@ -2,24 +2,20 @@ package com.newus.traders.user.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import com.newus.traders.user.entity.User;
+import com.newus.traders.user.entity.UserEntity;
+import java.util.List;
 
-import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-//    @EntityGraph(attributePaths = "authorities")
-//    Optional<User> findOneWithAuthoritiesByUsername(String username);
-   @EntityGraph(attributePaths = "authorities")
-   Optional<User> findOneWithAuthoritiesByEmail(String email);
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
 
-   	// Optional<User> findOneWithAuthoritiesByUsername(String email);
+	UserEntity findByEmail(String email);
 	Boolean existsByEmail(String email);
-	User findByEmailAndPassword(String email, String password);
-	User findByEmail(String email);
-	Optional<User> findByuserId(String userId);
+	UserEntity findByEmailAndPassword(String email, String password);
+	Optional<UserEntity> findById(String id);
 	boolean existsByUsername(String username);
 }
 
